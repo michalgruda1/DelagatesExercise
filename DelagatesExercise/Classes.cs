0010977af8f7d75d2574b5005261463817654003
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2018-09-11 15:36:55 +0200

    Add project files.

---
+++ DelagatesExercise/Classes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelagatesExercise
+{
+    // public delegate bool SendGifts(List<CustomerAddress>);
+
+    public class MarketingDepertment
+    {
+        public bool RunMarketingCampaign(decimal budget)
+        {
+            // SendGifts CampaignHandler;
+            bool success = false;
+            AddressProvider addressProvider = new AddressProvider();
+            List<CustomerAddress> customerAddresses = addressProvider.GetAddressesOfNewProspects();
+            if (budget > 10000)
+            {
+                CoffeeCupCompany someCoffeeCupCompany = new CoffeeCupCompany(); 
+                success = someCoffeeCupCompany.SendCoffeeCups(customerAddresses);
+                return success;
+            }
+            else
+            {
+                BallpenCompany someBallpenCompany = new BallpenCompany();
+                success = someBallpenCompany.SendBallPens(customerAddresses);
+                return success;
+            }
+        }
+    }
+
+    public class AddressProvider
+    {
+        public List<CustomerAddress> GetAddressesOfNewProspects()
+        {
+            List < CustomerAddress > listOfCustomerAddresses = new List<CustomerAddress>();
+            return listOfCustomerAddresses;
+        }
+    }
+
+    public class BallpenCompany
+    {
+        public bool SendBallPens(List<CustomerAddress> listOfCustomerAddresses)
+        {
+            // for each address send ballpen
+            return true;
+        }
+    }
+
+    public class CoffeeCupCompany
+    {
+        public bool SendCoffeeCups(List<CustomerAddress> listOfCustomerAddresses)
+        {
+            // for each address send coffee cup
+            return true;
+        }
+    }
+
+    public class CustomerAddress
+    {
+        // nothing now
+    }
+}
---
+++ DelagatesExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelagatesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success = false;
+            decimal budget = 10000m;
+
+            MarketingDepertment dept = new MarketingDepertment();
+            success = dept.RunMarketingCampaign(budget);
+
+            Console.WriteLine("The new marketing campaign has been a {0}", success ? "success" : "failure");
+            Console.Read();
+        }
+    }
+}
